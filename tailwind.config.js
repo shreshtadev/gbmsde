f0003a015a762822f5d83d59b1842d547b91bd98
Chinmai D Bharadwaj <chinmai.bharadwaj@ymail.com>
2023-10-14 12:13:25 +0530

    apply css modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ import defaultTheme from "tailwindcss/defaultTheme";
 import colors from "tailwindcss/colors";
 import forms from "@tailwindcss/forms";
 import typography from "@tailwindcss/typography";
-import preset from './vendor/filament/support/tailwind.config.preset';
+import preset from "./vendor/filament/support/tailwind.config.preset";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -17,33 +17,33 @@ export default {
     ],
 
     theme: {
-        colors: {
-            transparent: "transparent",
-            current: "currentColor",
-            black: colors.black,
-            blue: colors.blue,
-            cyan: colors.cyan,
-            emerald: colors.emerald,
-            fuchsia: colors.fuchsia,
-            slate: colors.slate,
-            gray: colors.gray,
-            neutral: colors.neutral,
-            stone: colors.stone,
-            green: colors.green,
-            indigo: colors.indigo,
-            lime: colors.lime,
-            orange: colors.orange,
-            pink: colors.pink,
-            purple: colors.purple,
-            red: colors.red,
-            rose: colors.rose,
-            sky: colors.sky,
-            teal: colors.teal,
-            violet: colors.violet,
-            yellow: colors.amber,
-            white: colors.white,
-        },
         extend: {
+            colors: {
+                transparent: "transparent",
+                current: "currentColor",
+                black: colors.black,
+                blue: colors.blue,
+                cyan: colors.cyan,
+                emerald: colors.emerald,
+                fuchsia: colors.fuchsia,
+                slate: colors.slate,
+                gray: colors.gray,
+                neutral: colors.neutral,
+                stone: colors.stone,
+                green: colors.green,
+                indigo: colors.indigo,
+                lime: colors.lime,
+                orange: colors.orange,
+                pink: colors.pink,
+                purple: colors.purple,
+                red: colors.red,
+                rose: colors.rose,
+                sky: colors.sky,
+                teal: colors.teal,
+                violet: colors.violet,
+                yellow: colors.amber,
+                white: colors.white,
+            },
             fontFamily: {
                 sans: ["Figtree", ...defaultTheme.fontFamily.sans],
             },
