46864b72dbdfb6de0f37721135c3162871df1e7f
Chinmai D Bharadwaj <chinmai.bharadwaj@ymail.com>
2023-10-14 12:36:12 +0530

    added footer to home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -864,6 +864,15 @@
             </div>
         </div>
     </div>
+    <footer
+        class="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">©
+            @php
+                echo date('Y');
+            @endphp
+            <a href="#" class="hover:underline">Shreshta.smg™</a>. All Rights Reserved.
+        </span>
+    </footer>
 </body>
 
 </html>
